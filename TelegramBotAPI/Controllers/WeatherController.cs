098784ab0ba9ea4ccea46688ccafb15edac540ec
JMKangas <jani.kangas@windowslive.com>
2025-04-23 20:39:30 +0300

    controller added

---
+++ TelegramBotAPI/Controllers/WeatherController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TelegramBotAPI.Services;
+
+namespace TelegramBotAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly WeatherService _weatherService;
+
+        public WeatherController(WeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWeather()
+        {
+            var data = await _weatherService.GetWeatherAsync();
+
+            var aika = DateTime.Now;
+
+            var temp = (double)1;
+            for (int i = 0; i < 23; i++)
+            {
+                aika = DateTime.Now;
+                if (aika.Hour == i)
+                {
+                    temp = data.Hourly.Temperature2M[i];
+                }
+            }
+
+            return Ok($"Lämpötila on; {temp}°C Jyväskylässä klo.{aika.ToShortTimeString()}");
+        }
+    }
+}
